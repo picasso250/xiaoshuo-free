8c33ee3912c25af63c6458c375d5678809b383eb
xiaochi <wxiaochi@qq.com>
2022-12-03 13:14:37 +0800

    y

--- build.py
+++ build.py
@@ -1,14 +1,15 @@
 import os
 
-ext=".html"
-for i in range(1,99999):
-    p=str(i)+ext
+ext = ".html"
+for i in range(1, 99999):
+    p = str(i)+ext
     if os.path.exists(p):
-        with open(p,encoding="utf-8") as f:
+        with open(p, encoding="utf-8") as f:
             for line in f.readlines():
-                title=line.strip()
-                # print(title)
-                print("<li> <a href=\"#{}\" data-index=\"0\">{}</a> </li>".format(i,title))
+                title = line.strip()
+                tpl = "<li> <a href=\"#{}\" data-index=\"0\">{}</a> </li>"
+                li = tpl.format(i, title)
+                print(li)
                 break
     else:
         break
